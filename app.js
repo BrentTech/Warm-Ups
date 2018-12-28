58da4346671d96d19d5c4786331181baff9a7b99
Brent Woodward <Brent.Woodward.1@gmail.com>
2018-12-27 19:01:14 -0800

    warm-up has warmed me up!

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9. 10];
+
+function forLoop(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    console.log('i', arr[i]);
+  }
+}
+
+function whileLoop(arr) {
+  let i = 0;
+  while(i < arr.length) {
+    console.log('i', arr[i]);
+    i++
+  }
+}
+
+function map(arr, callback) {
+  arr.
+}
+
+function filter(arr, callback) {
+
+}
+
+function reduce(arr, callback) {
+
+}
+
